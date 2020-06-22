53537bcedf9135e9602b94b5f32700add6d0897a
edson <edson.domenech@videplast.com.br>
2020-06-21 22:16:54 -0300

    teste 2

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -64,8 +64,7 @@ namespace QuickBuy.Web
                 if (env.IsDevelopment())
                 {
                     //spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
-                    //
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");                
                 }
             });
         }
