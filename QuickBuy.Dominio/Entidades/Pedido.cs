2ade31ac758954d3c65c292f07f6f8c42d0a57e8
Edson Domenech <edson.domenech@videplast.com.br>
2020-06-22 09:46:44 -0300

    19

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
@@ -9,6 +11,14 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
         /// <summary>
         /// Pedido deve ter pelo menos um item ou muitos itens
         /// </summary>
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,47 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool NaoFoifinido
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.NaoDefinido;
+
+            }
+        }
+        public bool EhBoleto
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Boleto;
+
+            }
+        }
+        public bool EhCartaoCredito
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.CartaoCredito;
+
+            }
+        }
+        public bool EhDeposito
+        {
+            get
+            {
+                return Id == (int)TipoFormaPagamentoEnum.Deposito;
+
+            }
+        }
+    }
+}
