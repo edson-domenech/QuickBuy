fa3d460d95365ab67cbc1d703f5b9434dd0c0b6d
Edson Domenech <edson.domenech@videplast.com.br>
2020-06-22 14:45:08 -0300

    39

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -14,7 +14,7 @@ namespace QuickBuy.Dominio.Entidades
         /// <summary>
         /// Usuário pode ter nenhum ou muitos pedidos
         /// </summary>
-        public ICollection<Pedido> Pedidos { get; set; }
+        public virtual ICollection<Pedido> Pedidos { get; set; }
 
         public override void Validate()
         {
