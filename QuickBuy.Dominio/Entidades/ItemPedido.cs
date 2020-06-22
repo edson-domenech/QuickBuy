ef43ab93f57403807111159485953a1d4c492a25
Edson Domenech <edson.domenech@videplast.com.br>
2020-06-22 11:26:24 -0300

    23

--- QuickBuy.Dominio/Entidades/ItemPedido.cs
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -9,7 +9,6 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public int ProdutoId { get; set; }
         public int Quantidade { get; set; }
-
         public override void Validate()
         {
             LimparMensagemValidacao();
