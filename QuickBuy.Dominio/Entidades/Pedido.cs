4bacce337a7d20d189dfe84891c10d097353f0c8
Edson Domenech <edson.domenech@videplast.com.br>
2020-06-22 09:26:54 -0300

    18

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        /// <summary>
+        /// Pedido deve ter pelo menos um item ou muitos itens
+        /// </summary>
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
