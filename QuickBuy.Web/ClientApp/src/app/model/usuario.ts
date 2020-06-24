5af070038e5be6589aa69aa51826e11cf8b02be0
Edson Domenech <edson.domenech@videplast.com.br>
2020-06-24 12:00:48 -0300

    88

---
+++ QuickBuy.Web/ClientApp/src/app/model/usuario.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+export class Usuario {
+    id: number;
+    email: string;
+    senha: string;
+    nome: string;
+    sobreNome: string;
+}
--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Usuario } from "src/app/model/usuario";
 
 @Component({
   selector: "app-login",
@@ -6,7 +7,21 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent {  
+  public imgTitle = "Título da imagem";
+  public usuario;  
+  //public usuarios = ["Usuário 1", "Usuário 2", "Usuário 3", "Usuário 4"];  
+  public usuarioAutenticado;
 
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    /*
+    alert(this.usuario.email + " - " + this.usuario.senha);
+    this.usuarioAutenticado = this.usuario.email == "a" && this.usuario.senha == "a";
+    */
+  }
 
 }
